f9d7348ea155dc4c21b328fa6b8c5b7734f9293c
maxcana <m.canarius@gmail.com>
2026-01-27 22:07:20 -0800

    logic complete
    all logic is finished, and its "blazingly fast"!!!!!!!!!!!
    
    the python solver was taking ~32500 seconds using 12 processes, and this rust is taking only ~16250s without multithreading.
    
    i haven't checked for bugs in the results and i haven't implemented entering guesses, but the logic is complete, and it runs!

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,28 @@
-use std::{collections::HashSet, fs, io::{self, Error}, usize};
+use std::{collections::HashMap, fs, io::{self, Error}, time, usize};
 
 // design:
 // enter guess and result (eg. "crane _gy__")
 // eliminate words that don't match the result
 // try all possible guesses and find the best one
 // the best one is the one that eliminates the most words on average
-// average is calculated by making the secret word every word on the list
+// average is calculated trying every secret word on the list
 
 // implementation:
-// enter guess
 // for every guess
 // for every secret word
-// calculate word eliminated
-// calculate average word eliminated
+// calculate words eliminated
+// calculate avg words eliminated
+// we'll validate possible secrets based on info using bitmaps
 
+// MARK: main
 
-// MARK: Bit stuff
-
-// what we are gonna do is encode the INFORMATION (guess & colors) with the 5 RULES so we can compare it with a bitwise AND to every POSSIBLE SECRET.
-// this way we only calculate the rules [1 times] while encoding, and do bitwise AND [2309 times] instead of calculating the rules [2309 times]
-// this is Ingenious
-
-// dogma function
-// bitmask = INFORMATION, bitmap = POSSIBLE SECRET
-
-fn load_words(file_path: &str) -> HashSet<String> {
+fn load_words(file_path: &str) -> Vec<String> {
     let content: Result<String, Error> = fs::read_to_string(file_path);
     match content {
         Ok(s) => {
-            let sm: String = s.trim().to_lowercase();
-            // map converts borrowed strings into new data
-            let set: HashSet<String> = sm.split("\n").map(|line: &str| line.to_string()).collect();
+            let sm: String = s.trim().to_ascii_lowercase();
+            // watch out for CRLF line endings, use .trim() each line
+            let set: Vec<String> = sm.split("\n").map(|line: &str| line.trim().to_string()).collect();
             set
         },
         Err(e) => {panic!("ur mega cooked buddy")}
@@ -42,56 +34,124 @@ fn input(query: &str) -> String{
     io::stdin().read_line(&mut user_input).expect("Failed to read user input");
     user_input
 }
-fn encode_word(input: &str) -> [u8; 5]{
-    input.as_bytes().iter().map(|byte| *byte - 4u8).collect::<Vec<u8>>().try_into().unwrap() 
-    // try_into doesnt take ::<> generic params. not clear why it interprets it (something with the trait), but cool.
+fn encode_word(input: &str) -> [u8; 5] {
+    input.to_ascii_lowercase().as_bytes().iter().map(|byte| *byte - 97u8).collect::<Vec<u8>>().try_into().unwrap() // try_into doesnt take ::<> generic params. not clear how it interprets it (something with the trait), but cool.
 }
-
+fn decode_word(input: &[u8; 5]) -> String {
+    String::from_utf8(input.map(|byte| byte + 97u8).to_vec()).unwrap()
+}
 fn main() {
-    let inp: String = input("Enter guess and result (eg. \"crane _gy__\"").trim().to_lowercase();
-    let vec: Vec<&str> = inp.split(" ").collect();
+    // let inp: String = input("Enter guess and result (eg. \"crane _gy__\"").trim().to_lowercase();
+    // let vec: Vec<&str> = inp.split(" ").collect();
 
-    let word: &str = vec[0];
-    let guess: &str = vec[1];
+    // let word: &str = vec[0];
+    // let guess: &str = vec[1];
 
-    let PG: HashSet<[u8; 5]> = load_words("wordle_words.txt").iter().map(|str: &String| encode_word(str)).collect();
+    let PG: Vec<[u8; 5]> = load_words("wordle_words.txt").iter().map(|str: &String| encode_word(str)).collect();
     let PS = PG.clone();
+    let bitmap_cache: HashMap<[u8; 5], [u128; 3]> = PS.iter().map(|&word| word).zip(PS.iter().map(|&word| build_bitmap(word))).collect(); // absolute cinema
 
+    let mut GS: HashMap<[u8; 5], f32> = HashMap::new();
+
+    let start_inst = time::Instant::now();
     for pg in &PG {
+        let mut total_elim: u32 = 0;
+
         for ps in &PS {
             let colors: [u8; 5] = get_colors(pg, ps);
-
-            // 1 bitmap is 26 * 11 bits = 282 bits
-            let mut bitmask: [u128; 3] = [0,0,0];
-
+            
+            let mut bitmask: [u128; 3] = [0,0,0]; // 1 bitmap is 26 * 11 bits = 282 bits
+            let mut minimum_of_ltr: [u8; 26] = [0; 26];
+            let mut ltrs_with_maximum: [bool; 26] = [false; 26];
+            
+            // encode "positions" section of bitmask, while setting up minimums and maximums for "count" section
             for i in 0..5usize {
                 let (letter, color) = (pg[i], colors[i]);
 
                 match color {
                     0 => { //gray
                         write_bit(&mut bitmask, letter, i);
+                        ltrs_with_maximum[pg[i] as usize] = true;
                     }
                     1 => { //yellow
                         write_bit(&mut bitmask, letter, i);
+                        minimum_of_ltr[pg[i] as usize] += 1;
                     }
-                    2 => { //green TODO
-                        //write_bit(&mut bitmask, letter, pos);
+                    2 => { //green
+                        write_row_excluding_bit(&mut bitmask, letter, i);
+                        minimum_of_ltr[pg[i] as usize] += 1;
                     }
                     _ => {panic!()}
-                }
-                
+                }  
             }
+            
+            // encode "count" section of bitmask
+            for i in 0..5usize {
+                let ltr = pg[i] as usize;
+                let min = minimum_of_ltr[ltr] as usize;
+                
+                // what quantities of this letter are disallowed? lets encode it
+                for count in 0..6 { 
+                    if ! if ltrs_with_maximum[ltr] {min == i} else {min <= i} {
+                        // count section is from row 5 - row 10
+                        write_bit(&mut bitmask, ltr as u8, count + 5)
+                    };
+                }
+            }
+
+            // bitmask is finished. lets compare it against every possible secret.
+
+            let mut elim: u32 = 0;
+            for (ps, bitmap) in &bitmap_cache {
+                if !bitmaps_match(&bitmask, bitmap) {
+                    elim += 1;
+                }
+            }
+            total_elim += elim;
         }
+        let avg_elim: f32 = total_elim as f32 / PS.len() as f32;
+        println!("guess {} eliminates {:.2} words on average", decode_word(pg).to_ascii_uppercase(), avg_elim);
+        GS.insert(*pg, avg_elim);
+
+        // time estimates
+        let amount_finished: f64 = GS.len() as f64 / PG.len() as f64;
+        let elapsed: f64 = start_inst.elapsed().as_secs_f64();
+        println!("estimated time remaining: {:.1}s", elapsed / amount_finished);
+
     }
-    
 
 }
 
-/// write a 1 bit into a bitmap or bitmask.
-/// letter = col (0-25), row = row (0-10).
-/// row includes position section (0-4) and count section (5-10)
-fn write_bit(map: &mut[u128; 3], letter: u8, row: usize){
-    write_raw_bit(map, 26 * row + letter as usize);
+// MARK: bit stuff
+
+// what we are gonna do is encode the INFORMATION (guess & colors) with the 5 RULES so we can compare it with a bitwise AND to every POSSIBLE SECRET.
+// this way we only calculate the rules [1 times] while encoding, and do bitwise AND [14855 times] instead of calculating the rules [14855 times]
+// this is Ingenious
+
+// dogma function // (we design our algorithm based on this being true)
+/// #### check whether a possible secret is valid based on encoded information
+/// bitmask: INFORMATION (encoded guess and colors). bitmap: possible secret to check validity of
+fn bitmaps_match(bitmask: &[u128; 3], bitmap: &[u128; 3]) -> bool {
+    bitmask.iter().zip(bitmap.iter()).all(|(&b, &m)| b & m == 0)
+}
+
+fn build_bitmap(ps: [u8; 5]) -> [u128; 3] {
+    let mut bitmap: [u128; 3] = [0,0,0];
+    
+    let mut ltr_count: [u8; 26] = [0; 26];
+
+    // encode "letter" section + counts prep
+    for (i,&ltr) in ps.iter().enumerate() {
+        write_bit(&mut bitmap, ltr, i);
+        ltr_count[i] += 1;
+    }
+
+    // encode "counts" section
+    for ltr in ps {
+        write_bit(&mut bitmap, ltr, 5 + ltr_count[ltr as usize] as usize);
+    }
+
+    return bitmap;
 }
 
 /// make a bit at position bitnum become 1
@@ -99,36 +159,57 @@ fn write_raw_bit(map: &mut[u128; 3], bitnum: usize){ map[bitnum / 128] = map[bit
 /// make a bit at position bitnum become 0
 fn delete_raw_bit(map: &mut[u128; 3], bitnum: usize){ map[bitnum / 128] = map[bitnum / 128] & !(1u128 << (bitnum % 128)) }
 
-/// write a solid segment of 1 bits into a bitmap or bitmask, using only left (inclusive) and right (inclusive) bound parameters
+/// #### write a solid segment of 1 bits into a bitmap or bitmask, using left (inclusive) and right (inclusive) bound parameters
+/// basically a workaround for not having a unsigned bigint. which is why this code looks intimidating, its just trying to make the array work as one biguint.
 fn write_raw_bit_sequence(map: &mut[u128; 3], left: usize, right: usize){
-    let first_arr_index = left / 128;
-    let second_arr_index: usize = right / 128;
-    if(first_arr_index == second_arr_index){
-        // actually write
+    let leftmost_seg = left / 128;
+    let rightmost_seg = right / 128;
+
+    if leftmost_seg == rightmost_seg {
+        // the write only affects one segment. lets actually write.
+        let seg = left / 128;
+        map[seg] = map[seg] | ((1u128 << (right-left+1)) - 1) << (128 - ((right % 128) + 1))
+        // ex: left = 2, right = 5. 
+        // 1 << (4) - 1 = 10000 - 1 = 1111. 1111 >> (left) = 001111. 001111 << (128 - (6 == right-left+1+left == right+1)) == 00111100000000000...
     } else {
-        // TODO
-        // write_raw_bit_sequence(map, left, final_bit_in_first_array);
-        // write_raw_bit_sequence(map, first_bit_in_second_array, right);
+        // the write affects multiple indices. recursive; calls this function for each segment affected.
+        for i in leftmost_seg..=rightmost_seg {
+            let seg_start = 128 * i;
+            let seg_end = 128 * (i + 1) - 1;
+
+            if i == leftmost_seg {
+                write_raw_bit_sequence(map, left, seg_end);
+            } else if i == rightmost_seg {
+                write_raw_bit_sequence(map, seg_start, right);
+            } else {
+                write_raw_bit_sequence(map, seg_start, seg_end);
+            }
+        }
     }
 }
 
-/// write an entire row of 1 bits into a bitmap or bitmask, except, leave a single column(letter) on the row as a 0.
-fn write_row_excluding_single(map: &mut[u128; 3], letter_excl: u8, row: usize){
-    let bitnum: usize = 26 * row + letter_excl as usize;
-    let bitnum_left: usize = 26 * row;
-    let bitnum_right: usize = 26 * (row+1)-1;
 
-    // make all the bits within bitnum_left and bitnum_right become 1
-    write_raw_bit_sequence(map, bitnum_left, bitnum_right);
-
-    // make the bit at bitnum become 0
-    delete_raw_bit(map, bitnum);
+/// #### write a 1 bit into a bitmap or bitmask.
+/// letter = col (0-25), row = row (0-10). row includes position section (0-4) and count section (5-10)
+fn write_bit(map: &mut[u128; 3], letter: u8, row: usize){
+    write_raw_bit(map, 26 * row + letter as usize);
 }
 
+/// write an entire row of 1 bits into a bitmap or bitmask, except, leave a single column(letter) on the row as a 0.
+fn write_row_excluding_bit(map: &mut[u128; 3], letter_excl: u8, row: usize){
+    // make all the bits within bitnum_left and bitnum_right become 1
+    write_raw_bit_sequence(map, 26 * row, 26 * (row+1)-1);
+
+    // make the bit at bitnum become 0
+    delete_raw_bit(map, 26 * row + letter_excl as usize);
+}
+
+// MARK: colors
 
 /// convert a guess and secret into colors (information for that guess).
 fn get_colors(guess: &[u8; 5], secret: &[u8; 5]) -> [u8; 5]{
     let mut colors: [u8; 5] = [0,0,0,0,0];
+
 
     // used for populating colors with yellows at the end. tracks how many yellows are needed
     let mut how_many_yellows: [u8; 26] = [0; 26];
